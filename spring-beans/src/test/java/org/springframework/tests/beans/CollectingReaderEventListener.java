9a1b58eff9ad67ce3e01ca5ccfa7cb7a6eb2787f
hujie <2580532608@qq.com>
2020-04-13 15:13:26 +0800

    Bean 的生命周期（从实例化到销毁）

--- spring-beans/src/test/java/org/springframework/tests/beans/CollectingReaderEventListener.java
+++ spring-beans/src/test/java/org/springframework/tests/beans/CollectingReaderEventListener.java
@@ -153,4 +153,19 @@ public class CollectingReaderEventListener implements ReaderEventListener {
 		beanPostProcessorTest.display();
 
 	}
+
+	@Test
+	public void iocGetBeanAll() {
+		ClassPathResource resource = new ClassPathResource("org/springframework/beans/factory/ClassPathXmlApplicationContextTests-resourceImport.xml");
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions(resource);
+		// BeanFactory 容器一定要调用该方法进行 BeanPostProcessor 注册
+		factory.addBeanPostProcessor(new LifeCycleBean());
+		LifeCycleBean lifeCycleBean = (LifeCycleBean)factory.getBean("lifeCycle");
+		lifeCycleBean.display();
+		System.out.println("方法调用完成，容器开始关闭....");
+// 关闭容器
+		factory.destroySingletons();
+	}
 }
